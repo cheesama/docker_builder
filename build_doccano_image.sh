2385aceaa72166094b8b895adc7f23add77a7e8e
Kion Kim <kion.kim@gmail.com>
2021-02-18 13:51:23 +0900

    modify doccano build logic using container

--- build_doccano_image.sh
+++ build_doccano_image.sh
@@ -1,4 +1,6 @@
-git clone https://github.com/doccano/doccano.git && \
-cd doccano && \
-docker build -t doccano_docker_image .
-
+docker pull doccano/doccano
+docker container create --name doccano \
+  -e "ADMIN_USERNAME=lina" \
+  -e "ADMIN_EMAIL=admin@example.com" \
+  -e "ADMIN_PASSWORD=lina_dna" \
+  -p 8000:8000 doccano/doccano
